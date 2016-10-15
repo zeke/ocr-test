fc780f29f93c2ca698ee980bef0614ad355b554a
Zeke Sikelianos <zeke@sikelianos.com>
2016-10-14 22:48:05 -0700

    add start script

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const Tesseract = require('tesseract.js')
 
-Tesseract.recognize(path.join(__dirname, 'list.jpg'))
+const file = require('fs').readFileSync(path.join(__dirname, 'list.jpg'))
+
+Tesseract.recognize(file)
   .progress(message => console.log(message))
   .catch(err => console.error(err))
   .then(result => console.log(result))
